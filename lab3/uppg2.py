b8fe40a2f77d68fb0c1b7971d83a6e2887eefd37
Arvid Roos <arvroo@ug.kth.se>
2026-01-27 13:41:35 +0100

    nu funkar uppg2

--- lab3/uppg2.py
+++ lab3/uppg2.py
@@ -7,4 +7,19 @@ with open("word3.txt", "r", encoding = "utf-8") as svenskfil:
             print(ordet, end = " ") 
         else:
             svenska.put(ordet)             # in i sökträdet
+            
+    print("\n")
+    with open("engelska.txt", "r", encoding = "utf-8") as engelskfil:
+        engelska_ord = []
+        for rad in engelskfil:
+            raden = rad.strip().split()
+            engelska_ord = engelska_ord + raden
+        engelska = Bintree()
+        for ord in engelska_ord:
+            if ord in engelska:
+                pass
+            else:
+                if ord in svenska:
+                    engelska.put(ord)
+                    print(ord, end = " ")
 print("\n")

a4b921aa176879843f9740ce52cdd65be8fcf837
Arvid Roos <arvroo@ug.kth.se>
2026-02-04 21:27:33 +0100

    lab4 och lab5 klara
    ef

---
+++ lab5/BintreeFile.py
@@ -0,0 +1,138 @@
+class Node:
+    def __init__(self, value, left=None, right=None):
+        self.value = value
+        self.left = left
+        self.right = right
+
+    def __str__(self):
+        return str(self.value)
+
+
+class Bintree:
+    def __init__(self):
+        self.root = None
+
+    def __len__(self, p="root"):
+        if p == "root":
+            p = self.root
+        if p is None:
+            return 0
+        return 1 + self.__len__(p.left) + self.__len__(p.right)
+
+    def _depth_(self, p=None):
+        if p is None:
+            p = self.root
+        return self._depth_helper(p) - 1
+
+    def _depth_helper(self, p):
+        if p is None:
+            return 0
+        return 1 + max(self._depth_helper(p.left), self._depth_helper(p.right))
+
+    def write(self, p="root"):
+        if p == "root":
+            p = self.root
+        if p is not None:
+            self.write(p.left)
+            print(p)
+            self.write(p.right)
+
+    def preorder(self, p="root", depth=0):
+        if p == "root":
+            p = self.root
+        if p is not None:
+            print("  " * depth + str(p.value))
+            self.preorder(p.left, depth + 1)
+            self.preorder(p.right, depth + 1)
+
+    def postorder(self, p="root"):
+        if p == "root":
+            p = self.root
+        if p is not None:
+            self.postorder(p.left)
+            self.postorder(p.right)
+            print(p)
+
+    def level_index(self, p="root", depth=0):
+        if p == "root":
+            p = self.root
+        if p is None:
+            return None
+        if p is not None:
+            output = [(depth, p.value)]
+            tmp1 = self.level_index(p.left, depth + 1)
+            if tmp1 is not None:
+                output = output + tmp1
+            tmp2 = self.level_index(p.right, depth + 1)
+            if tmp2 is not None:
+                output = output + tmp2
+            return output
+
+    def tree_list(self):
+        print("\n")
+        tuplarna = self.level_index()
+        max_depth = self._depth_() + 1
+        levels = [[] for _ in range(max_depth)]
+
+        for depth, val in tuplarna:
+            levels[depth].append(val)
+
+        i = 0
+        for level in levels:
+            print("  " * (max_depth - i), level)
+            i += 1
+
+    def inorderlist(self, p="root"):
+        if p == "root":
+            p = self.root
+        if p is None:
+            return []
+        return self.inorderlist(p.left) + [p.value] + self.inorderlist(p.right)
+
+    def __contains__(self, val, p="root"):
+        if p == "root":
+            p = self.root
+        if p is None:
+            return False
+        if p.value == val:
+            return True
+
+        if p.value > val:
+            return self.__contains__(val, p.left)
+        if p.value < val:
+            return self.__contains__(val, p.right)
+
+    def put(self, val, p="root"):
+        if self.root is None:
+            self.root = Node(val)
+            return
+        if p == "root":
+            p = self.root
+        if val <= p.value:
+            if p.left is None:
+                p.left = Node(val)
+                return
+            self.put(val, p.left)
+        else:
+            if p.right is None:
+                p.right = Node(val)
+                return
+            self.put(val, p.right)
+
+    def makeTree(self, my_list, depth=0):
+        if not my_list:
+            return None
+        length = len(my_list)
+        middle = length // 2
+        tmp = Node(my_list[middle])
+        if depth == 0:
+            self.root = tmp
+        if len(my_list) == 1:
+            return tmp
+        tmp.left = self.makeTree(my_list[:middle], depth + 1)
+        tmp.right = self.makeTree(my_list[middle + 1 :], depth + 1)
+        return tmp
+
+    def makeBalanced(self):
+        my_list = self.inorderlist()
+        self.makeTree(my_list)
---
+++ lab5/bfs.py
@@ -0,0 +1,67 @@
+from BintreeFile import Bintree
+from linkedQFile import LinkedQ
+
+
+class ParentNode:
+    def __init__(self, word, parent=None):
+        self.word = word
+        self.parent = parent
+
+
+def makechildren(nod, q, lista, gamla):
+
+    bokstäver = "abcdefghijklmnopqrstuvxyzåäö"
+
+    sökord = nod.word
+
+    for i in range(len(sökord)):
+        for bokstav in bokstäver:
+            nytt_barn = sökord[:i] + bokstav + sökord[i + 1 :]
+            if nytt_barn != sökord and nytt_barn not in gamla and nytt_barn in lista:
+                tmp = ParentNode(nytt_barn, nod)
+                q.enqueue(tmp)
+                gamla.put(nytt_barn)
+
+
+def writechain(nod):
+    print(nod.word)
+    if nod.parent == None:
+        return
+    writechain(nod.parent)
+
+
+svenska = Bintree()
+gamla = Bintree()
+
+with open("word3.txt", "r", encoding="utf-8") as svenskfil:
+    for rad in svenskfil:
+        ordet = rad.strip()
+        if ordet in svenska:
+            pass
+        else:
+            svenska.put(ordet)
+
+    svenska.makeBalanced()
+
+    q = LinkedQ()
+
+    startord = "par"
+    söktord = "äng"
+    found = False
+
+    start = ParentNode(startord)
+
+    q.enqueue(start)
+
+    while not q.isEmpty():
+        nod = q.dequeue()
+        if nod.word == söktord:
+            found = True
+            slutordsnod = nod
+            break
+        makechildren(nod, q, svenska, gamla)
+
+    if not found:
+        print("Ingen väg hittades")
+    else:
+        writechain(slutordsnod)

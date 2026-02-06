ec87e5beaa85914b6d320b9b507cbdc236b86a9d
Arvid Roos <arvroo@ug.kth.se>
2026-02-06 14:38:17 +0100

    småfix

--- lab6/main.py
+++ lab6/main.py
@@ -9,19 +9,31 @@ halvLista = song_list[0:500000]
 kvartsLista = song_list[0:250000]
 
 
-def linj_sok(list, namn):
-    n = len(list)
-    print("Antal element =", n)
-    sista = list[n - 1]
-    testartist = sista.artist
+def linj_sok(lista, namn):
+    n = len(lista)
+    print("\nAntal element =", n)
+    sista = lista[n - 1]
+    testsong = sista.title
     print(namn)
-    linjtid = timeit.timeit(stmt=lambda: linsok(list, testartist), number=10000)
+    linjtid = timeit.timeit(stmt=lambda: linsok(lista, testsong), number=1000)
     print("Linjärsökningen tog", round(linjtid, 4), "sekunder")
 
 
-linj_sok(song_list, "Hela listan")
-linj_sok(halvLista, "Halva listan")
-linj_sok(kvartsLista, "Kvarts lista")
+def bin_sok(lista, namn):
+    n = len(lista)
+    print("\nAntal element =", n)
+    sista = lista[n - 1]
+    testsong = sista.title
+    print(namn)
+    linjtid = timeit.timeit(stmt=lambda: binsok(lista, testsong), number=1000)
+    print("Linjärsökningen tog", round(linjtid, 4), "sekunder")
+
+
+# linj_sok(kvartsLista, "Kvarts lista")
+# linj_sok(halvLista, "Halva listan")
+# linj_sok(song_list, "Hela listan")
+
+bin_sok(song_list, "Hela listan")
 
 # for song in song_list[:100]:
 # print(song)

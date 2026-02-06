75a0643f39587cda00686d51f870e0cebb5595ed
Andreas Hansson <andreashansson@n146-p59.eduroam.kth.se>
2026-02-06 14:44:34 +0100

    lab6 fix

--- lab6/SearchFile.py
+++ lab6/SearchFile.py
@@ -1,16 +1,32 @@
 from ClassFile import *
+
+
 def linsok(lista, key):
     for song in lista:
         if song.artist == key:
             return song
     return None
-    
+
+
+def binsok(lista, key):
+    length = len(lista)
+    middle = length // 2
+    title = lista[middle].title
+    if key == title:
+        return lista[middle]
+    if key > title:
+        return binsok(lista[middle + 1 :], key)
+    if key < title:
+        return binsok(lista[:middle], key)
+
 
 def readfile(filename):
     lista = []
     with open(f"{filename}", "r", encoding="utf-8") as songfil:
         for rad in songfil:
             delar = rad.strip().split("<SEP>")
-            song = Song(trackid=delar[0], time=delar[1], artist=delar[2], title=delar[3])
+            song = Song(
+                trackid=delar[0], time=delar[1], artist=delar[2], title=delar[3]
+            )
             lista.append(song)
         return lista
--- lab6/main.py
+++ lab6/main.py
@@ -13,15 +13,26 @@ def linj_sok(list, namn):
     n = len(list)
     print("Antal element =", n)
     sista = list[n - 1]
-    testartist = sista.artist
+    testsong = sista.title
     print(namn)
-    linjtid = timeit.timeit(stmt=lambda: linsok(list, testartist), number=10000)
+    linjtid = timeit.timeit(stmt=lambda: linsok(list, testsong), number=100)
     print("Linjärsökningen tog", round(linjtid, 4), "sekunder")
 
 
-linj_sok(song_list, "Hela listan")
-linj_sok(halvLista, "Halva listan")
-linj_sok(kvartsLista, "Kvarts lista")
+def bin_sok(list, namn):
+    n = len(list)
+    sista = list[n - 1]
+    testsong = sista.title
+    list.sort()
+    print(namn)
+    bintid = timeit.timeit(stmt=lambda: binsok(list, testsong), number=100)
+    print("Binärsökningen tog", round(bintid, 4), "sekunder")
+
+
+# linj_sok(song_list, "Hela listan")
+# linj_sok(halvLista, "Halva listan")
+# linj_sok(kvartsLista, "Kvarts lista")
+bin_sok(song_list, "Hela listan")
 
 # for song in song_list[:100]:
 # print(song)

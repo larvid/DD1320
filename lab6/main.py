17adb42b6b7aee2bb24ce71fd22ec882379615b0
Arvid Roos <arvroo@ug.kth.se>
2026-02-06 14:04:47 +0100

    mindre hårdkodning

--- lab6/main.py
+++ lab6/main.py
@@ -1,31 +1,27 @@
-from ClassFile import * 
-from SearchFile import * 
+from ClassFile import *
+from SearchFile import *
 
-import timeit 
+import timeit
 
 filename = "unique_tracks.txt"
 song_list = readfile(filename)
 halvLista = song_list[0:500000]
 kvartsLista = song_list[0:250000]
 
-n = len(song_list)
-print("Antal element =", n)
-sista = song_list[n-1]
-testartist = sista.artist
-print("Hel lista")
-tider = []
-for _ in range(10):
-    linjtid = timeit.timeit(stmt = lambda: linsok(song_list, testartist), number = 10000)
-    tider.append(linjtid)
-    #print("Linjärsökningen tog", round(linjtid, 4) , "sekunder")
-med_tid = sum(tider) / len(tider)
-print("Medel-linjärsökningen tog", round(med_tid, 4) , "sekunder")
+
+def linj_sok(list, namn):
+    n = len(list)
+    print("Antal element =", n)
+    sista = list[n - 1]
+    testartist = sista.artist
+    print(namn)
+    linjtid = timeit.timeit(stmt=lambda: linsok(list, testartist), number=10000)
+    print("Linjärsökningen tog", round(linjtid, 4), "sekunder")
 
 
+linj_sok(song_list, "Hela listan")
+linj_sok(halvLista, "Halva listan")
+linj_sok(kvartsLista, "Kvarts lista")
 
-
-
-
-#for song in song_list[:100]:
-    #print(song)
-
+# for song in song_list[:100]:
+# print(song)

bb08356f8c82f7dd7bbb760c59206d8539f139e2
Andreas Hansson <andreashansson@n146-p59.eduroam.kth.se>
2026-02-06 14:26:44 +0100

    bajjjjs

--- lab6/ClassFile.py
+++ lab6/ClassFile.py
@@ -1,14 +1,12 @@
-class Song: 
+class Song:
     def __init__(self, trackid, time, artist, title):
-        self.trackid = trackid 
+        self.trackid = trackid
         self.time = time
         self.artist = artist
-        self.title = title 
+        self.title = title
 
     def __lt__(self, other):
-        return self.artist < other.artist
-    
+        return self.title < other.title
+
     def __str__(self):
         return f"{self.artist}, {self.title}"
-
-        
